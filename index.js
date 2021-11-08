058707d1a37f3fd5b9ca95bb4798b1741575ac1d
Saurav KumaR <chunnu.bihat@gmail.com>
2021-11-08 12:14:28 +0530

    added help for conversions, and decided structure #3

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 import Conversion from './conversion/conversion.js';
-import Arithmatic from './arithmatic/arithmatic.js';
+import Arithmatic from './arithmetic/arithmetic.js';
 import Display from './display/display.js';
+import Util from './util/util.js';
 
 var arg = process.argv;
 
 if(arg[2] == '-c'){
-    if(arg[3] == '-m2n'){
-        if(arg[4] == '-time'){
+    if(arg[3] == 'm2n'){
+        if(arg[4] == 'time'){
             Conversion.m2nTime();
-        } else if(arg[4] == '-date'){
+        } else if(arg[4] == 'date'){
             Conversion.m2nDate();
         } else {
             console.log('default');
         }
     }
-    if(arg[3] == '-n2m'){
-        if(arg[4] == '-time'){
+    if(arg[3] == 'n2m'){
+        if(arg[4] == 'time'){
             Conversion.n2mTime();
-        } else if(arg[4] == '-date'){
+        } else if(arg[4] == 'date'){
             Conversion.n2mDate();
         } else {
             console.log('default');
@@ -26,21 +27,23 @@ if(arg[2] == '-c'){
 } else if(arg[2] == '-now'){
     Display.show();
 } else if(arg[2] == '-a'){
-    if(arg[3] == '-time'){
+    if(arg[3] == 'time'){
         Arithmatic.addTime();
-    } else if(arg[3] == '-date'){
+    } else if(arg[3] == 'date'){
         Arithmatic.addDate();
     } else{
         console.log('default');
     }
 } else if(arg[2] == '-s'){
-    if(arg[3] == '-time'){
+    if(arg[3] == 'time'){
         Arithmatic.subTime();
-    } else if(arg[3] == '-date'){
+    } else if(arg[3] == 'date'){
         Arithmatic.subDate();
     } else{
         console.log('default');
     }
+} else if(arg[2] == '-h'){
+    Util.help();
 } else {
-    console.log('default');
+    Util.invalidArguments(arg[2]);
 }
---
+++ util/util.js
@@ -0,0 +1,55 @@
+class Util{
+    constructor(){
+        
+    }
+    static cmd = {
+        format: {
+            normal: {
+                date: 'dd-MM-yyyy',
+                time: 'hh:mm:ss'
+            },
+            metric: {
+                date: 'ddd-MM-yyyy',
+                time: 'hh:mm:ss'
+            },
+        },
+        help: 'node index.js -h',
+        conversions: {
+            main: 'node index.js -c <conversion-type> [time|date] ${time|date}',
+            conversion_type: {
+                m2n: 'm2n',
+                n2m: 'n2m',
+            },
+        },
+        // arithmatics: {
+        //     main: 'node index.js -c <arithmetic-type> [time|date] ${time|date} ${days|mints}'
+        // }
+
+    }
+
+    static invalidArguments(arg){
+        console.log('\n`'+arg+'` isn\'t a valid arguments.');
+        console.log('\nUse `'+this.cmd.help+'` command to know more about available commands.');
+    }
+    static help(){
+        this.desc();
+        console.log('\n1) '+this.cmd.help+'\t:show help');
+
+        console.log('\n2) Format\tMetric\t\tNormal');
+        console.log('   Date\t\t'+this.cmd.format.metric.date+'\t'+this.cmd.format.normal.date);
+        console.log('   Time\t\t'+this.cmd.format.metric.time+'\t'+this.cmd.format.normal.time);
+
+        console.log('\n4) '+this.cmd.conversions.main+'\t=> Convert metric datetime system to normal datetime system');
+        console.log('\t conversion-type\t: '+this.cmd.conversions.conversion_type.m2n+'\t => Convert metric system to normal system');
+        console.log('\t conversion-type\t: '+this.cmd.conversions.conversion_type.n2m+'\t => Convert normal system to metric system');
+        console.log('\t [time|date]\t\t: '+'time'+'\t => Convert time from one system to other');
+        console.log('\t [time|date]\t\t: '+'date'+'\t => Convert date from one system to other');
+        console.log('\t ${time|date}\t\t: '+''+'\t => Date/Time value in specified format to convert');
+        
+    }
+    static desc(){
+        console.log('**A js library to work with metric time system**');
+    }
+}
+
+export default Util;
