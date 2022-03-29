a3f37263ff474e9d0d279358406064a4991eddbc
Saurav KumaR <chunnu.bihat@gmail.com>
2022-03-29 22:30:18 +0530

    V1.2.0 (#43)
    * #41 changed format keyword
    
    * #41 changed format keyword

--- cli-lib/util/util.js
+++ cli-lib/util/util.js
@@ -40,7 +40,7 @@ class Util{
             format_keywords: {
                 hour: '%H',
                 minute: '%M',
-                seconds: '%D',
+                seconds: '%S',
                 day: '%d',
                 month: '%m',
                 year: '%y',
@@ -97,7 +97,7 @@ class Util{
         this.version();
     }
     static version(){
-        console.log(' v1.0.0');
+        console.log(' v1.2.0');
     }
     static checkInputDateFormat(date, system){
         const element = date.split('-');
